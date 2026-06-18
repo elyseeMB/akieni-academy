0bd20a6fc92b3b198b7c4ec2faaae379ba1d1bb3
elysee <mboussaemmanuelito@gmail.com>
2026-06-18 02:15:41 +0100

    chore: setup CI/CD

---
+++ .github/script/getMeta.mjs
@@ -0,0 +1,35 @@
+import { readFileSync, writeFileSync, readdirSync } from "fs";
+import { parse } from "node-html-parser";
+
+function getFolders() {
+  return readdirSync(".").filter((dir) => /^tp\d+/.test(dir));
+}
+
+function readHTML(folder) {
+  return readFileSync(`${folder}/index.html`, "utf-8");
+}
+
+function extractTitle(root) {
+  return root.querySelector("title")?.text ?? "";
+}
+
+function extractDescription(root) {
+  return (
+    root.querySelector('meta[name="description"]')?.getAttribute("content") ??
+    ""
+  );
+}
+
+function buildTP(folder) {
+  const root = parse(readHTML(folder));
+  return {
+    name: folder,
+    title: extractTitle(root),
+    description: extractDescription(root),
+    url: `https://${folder}.akieniacademy.eembouz.com`,
+  };
+}
+
+const tps = getFolders().map(buildTP);
+
+writeFileSync("listing.json", JSON.stringify({ tps }));
