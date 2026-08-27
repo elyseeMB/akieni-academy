d7a27d24d241d0ffadc2bec7f85ee178c3b93f2a
elysee <mboussaemmanuelito@gmail.com>
2026-08-27 04:47:33 +0100

    feat(tp8): add  popover component

--- tp8/index.js
+++ tp8/index.js
@@ -1,11 +1,13 @@
 import { CalendarMovies } from "./wc/Calendar-movies.js";
 import { HeadCalendar } from "./wc/head-calendar.js";
+import { MoviePopover } from "./wc/movie-popover.js";
 import { PreviousItem } from "./wc/previous-item.js";
 import { SearchComponent } from "./wc/search-component.js";
 import { SuggestionComponent } from "./wc/suggestion-component.js";
 
 customElements.define("head-calendar", HeadCalendar);
 customElements.define("calendar-movies", CalendarMovies);
+customElements.define("movie-popover", MoviePopover);
 customElements.define("previous-item", PreviousItem);
 customElements.define("suggestion-component", SuggestionComponent);
 customElements.define("search-component", SearchComponent);
